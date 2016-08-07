3fa2bbfefb3a1839fc5fe267f22bec85567f4c26
Russell Pierce <russell.s.pierce@gmail.com>
2016-08-07 07:55:13 +0000

    add timestamp support

--- R/redshift-create.R
+++ R/redshift-create.R
@@ -3,7 +3,19 @@
 #' @param table_name \code{character}
 #' @export
 rs_create_table <- function(.data, table_name) {
-  data_types <- recode(unlist(lapply(.data, class)),
+  browser()
+  classes <- lapply(.data, class)
+  classes_first_pass <- lapply(classes, function(x) {
+    if (all(c("POSIXct", "POSIXt") %in% x)) {
+      x <- "TIMESTAMP"
+    }
+    return(x)
+  })
+  if (any("factor" %in% classes_first_pass)) {
+    warning("one of the columns is a factor")
+  }
+  data_types <- recode(unlist(classes_first_pass),
+         factor = "VARCHAR(255)",
          numeric = "FLOAT8",
          integer = "BIGINT",
          character = "VARCHAR(255)")
