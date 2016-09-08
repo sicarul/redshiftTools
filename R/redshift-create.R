2d4cb077bcf51d89423d85f91b467273e26e53e4
Russell Pierce <russell.s.pierce@gmail.com>
2016-09-08 15:34:06 +0000

    Respect reserved words

--- R/redshift-create.R
+++ R/redshift-create.R
@@ -1,6 +1,8 @@
 #' Create redshift table
 #' @param .data \code{data.frame}
 #' @param table_name \code{character}
+#'
+#' @return The column names that redshift actually ended up using
 #' @export
 rs_create_table <- function(.data, dbcon, table_name) {
   classes <- lapply(.data, class)
@@ -18,7 +20,14 @@ rs_create_table <- function(.data, dbcon, table_name) {
          numeric = "FLOAT8",
          integer = "BIGINT",
          character = "VARCHAR(255)")
-  spec <- paste(paste(names(.data), data_types), collapse=", ")
+
+  # Identify and mutate column names
+  column_names <- names(.data)
+  column_name_is_reserved <- column_names %in% tolower(RESERVED_WORDS)
+  column_names[column_name_is_reserved] <- paste0('rw_', column_names[column_name_is_reserved])
+
+  spec <- paste(paste(column_names, data_types), collapse=", ")
   sql_code <- whisker::whisker.render("CREATE TABLE IF NOT EXISTS {{table_name}} ({{spec}})")
   DBI::dbGetQuery(dbcon, sql_code)
+  return(column_names)
 }
