50836a73b373f5994af12574918c0f27b3f51647
statwonk <cpeter9@gmail.com>
2016-10-24 15:19:07 +0000

    Test that we can make a very simple connection, definitely not bulletproof. For example, what if we remove `event`?

---
+++ tests/testthat/test_connection_to_redshift.R
@@ -0,0 +1,10 @@
+context("Test that we can make a connection to Redshift")
+
+zapieR::make_db_connections()
+
+test_that(
+  "We can make a connection to Redshift",
+  expect_equal(
+    nrow(DBI::dbGetQuery(rs$con, "select * from event limit 1")),
+    1)
+)
