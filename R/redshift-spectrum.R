dd6b8916f91ba898d824bdec67015052541b251f
russellpierce <russell.s.pierce@gmail.com>
2017-09-29 15:49:25 +0000

    Don't drop non-existant parts

--- R/redshift-spectrum.R
+++ R/redshift-spectrum.R
@@ -11,6 +11,13 @@
 #' @importFrom DBI dbExecute
 spectrum_add_partition <- function(dbcon, table_name, part_name, part_value, base_location) {
   stopifnot("ident" %in% class(table_name))
-  dbExecute(dbcon, paste0(glue("alter table {table_name} drop partition({part_name}='{part_value}');"),
-  glue("alter table {table_name} add partition({part_name}='{part_value}') location '{base_location}/{part_name}={part_value}'")))
+
+  # if the partition already exists, drop it
+  table_parts <- strsplit(table_name, '.', fixed = TRUE)[[1]]
+  matching_partitions <- nrow(dbGetQuery(dbcon, glue("select * from SVV_EXTERNAL_PARTITIONS where schemaname = '{table_parts[1]}' and tablename = '{table_parts[2]}' and values ILIKE '%{part_value}%'")))
+  if (matching_partitions >= 1) {
+    dbExecute(dbcon, glue("alter table {table_name} drop partition({part_name}='{part_value}')"))
+  }
+
+  dbExecute(dbcon, glue("alter table {table_name} add partition({part_name}='{part_value}') location '{base_location}/{part_name}={part_value}'"))
 }
