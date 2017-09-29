4851c9d243dfb121376229b77e6a45a01879b7e5
russellpierce <russell.s.pierce@gmail.com>
2017-09-29 15:01:07 +0000

    Add check for invalid table name

--- R/redshift-create.R
+++ R/redshift-create.R
@@ -4,10 +4,14 @@
 #'
 #' @return The column names that redshift actually ended up using
 #' @importFrom DBI dbExecute
-#' @importFrom whisker whisker.render 
+#' @importFrom whisker whisker.render
 #' @export
 rs_create_table <- function(.data, dbcon, table_name, ...) {
   data_types <- identify_rs_types(.data)
+  # Check table name
+  if (grepl("-", table_name, fixed = TRUE)) {
+    stop("Hyphen in table name not allowed")
+  }
   # Identify and mutate column names
   column_names <- names(.data)
   column_name_is_reserved <- column_names %in% tolower(RESERVED_WORDS)
