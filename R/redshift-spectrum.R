8958399ad3698c26b48ad2c9953f025372124a04
russellpierce <russell.s.pierce@gmail.com>
2017-10-10 17:46:06 +0000

    Add spectrum_drop_partition()

--- R/redshift-spectrum.R
+++ R/redshift-spectrum.R
@@ -1,3 +1,29 @@
+#' Remove a partition from Redshift Spectrum
+#'
+#' @param dbcon The database connection to Redshift
+#' @param table_name A dblyr::in_schema defined table name
+#' @param part_name The name of the partition
+#' @param part_value The value of the partition
+#' @export
+#' @importFrom glue glue
+#' @importFrom DBI dbExecute
+spectrum_drop_partition <- function(dbcon, table_name, part_name, part_value) {
+  stopifnot("ident" %in% class(table_name))
+
+  # if the partition already exists, drop it
+  table_parts <- strsplit(table_name, '.', fixed = TRUE)[[1]]
+  matching_partitions <- nrow(dbGetQuery(dbcon, glue("select * from SVV_EXTERNAL_PARTITIONS where schemaname = '{table_parts[1]}' and tablename = '{table_parts[2]}' and values ILIKE '%{part_value}%'")))
+  if (matching_partitions >= 1) {
+    sql_code <- glue("alter table {table_name} drop partition({part_name}='{part_value}')")
+    log_if_verbose("Dropping partition: ", sql_code)
+    dbExecute(dbcon, sql_code)
+  } else {
+    log_if_verbose(glue("Partition already absent, {part_name}='{part_value}'"))
+  }
+  NULL
+}
+
+
 #' Add a partition to Redshift Spectrum
 #'
 #' @param dbcon The database connection to Redshift
@@ -6,7 +32,7 @@
 #' @param part_value The value of the partition
 #' @param base_location The base s3:// location of the partition files. It is assumed that the partition is specified as {base_location}/{part_name}={part_value}
 #'
-#' @return The result of the dbExecute for the command
+#' @return NULL
 #' @export
 #' @importFrom glue glue
 #' @importFrom DBI dbExecute
@@ -17,10 +43,13 @@ spectrum_add_partition <- function(dbcon, table_name, part_name, part_value, bas
   table_parts <- strsplit(table_name, '.', fixed = TRUE)[[1]]
   matching_partitions <- nrow(dbGetQuery(dbcon, glue("select * from SVV_EXTERNAL_PARTITIONS where schemaname = '{table_parts[1]}' and tablename = '{table_parts[2]}' and values ILIKE '%{part_value}%'")))
   if (matching_partitions >= 1) {
-    dbExecute(dbcon, glue("alter table {table_name} drop partition({part_name}='{part_value}')"))
+    log_if_verbose(glue("Partition already exists, {part_name}='{part_value}'"))
+  } else {
+    sql_code <- glue("alter table {table_name} add partition({part_name}='{part_value}') location '{base_location}/{part_name}={part_value}'")
+    log_if_verbose("Establishing partition: ", sql_code)
+    dbExecute(dbcon, sql_code)
   }
-
-  dbExecute(dbcon, glue("alter table {table_name} add partition({part_name}='{part_value}') location '{base_location}/{part_name}={part_value}'"))
+  NULL
 }
 
 #' Define the DDL for an External Table
