2511426f34d33fe8922435b84679627c6264a1a1
russellpierce <russell.s.pierce@gmail.com>
2017-10-25 17:25:50 +0000

    Add a couple depends

--- R/redshift-utils.R
+++ R/redshift-utils.R
@@ -240,12 +240,14 @@ identify_rs_types <- function (.data, character_length = NA_real_)
 #' @importFrom stringr str_trim
 #' @importFrom dplyr arrange desc select collect
 #' @importFrom purrr map
+#' @importFrom whisker whisker.render
+#' @importFrom magrittr %>%
 recent_errors <- function(con, n = 10) {
-  dbGetQuery(con, whisker.render("select colname, type, raw_field_value, err_reason, starttime from stl_load_errors limit {{limit}}",
+  dbGetQuery(con, whisker.render("select colname, type, raw_field_value, err_reason, starttime from [stl_load_errors order by starttime desc limit {{limit}}",
         list(
           limit = n
         )
-  )) %>% arrange(desc(starttime)) %>%
+  )) %>%
     select(-starttime) %>%
     collect(n = Inf) %>%
     purrr::map(stringr::str_trim) %>%
