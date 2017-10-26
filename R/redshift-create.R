cd0f836f52b8089dcaaeec0e6f6bea4e45b2bf4a
russellpierce <russell.s.pierce@gmail.com>
2017-10-26 18:54:27 +0000

    Split function into smaller parts

--- R/redshift-create.R
+++ R/redshift-create.R
@@ -8,16 +8,26 @@
 #' @export
 rs_create_table <- function(.data, dbcon, table_name, ...) {
   warnifnoschema(table_name)
-  data_types <- identify_rs_types(.data)
+  spec <- rs_create_table_spec(.data)
+  sql_code <- whisker::whisker.render("CREATE TABLE IF NOT EXISTS {{table_name}} {{spec}}")
+  DBI::dbExecute(dbcon, sql_code)
+  return(column_names)
+}
+
+#' Creates an Redshift column spec given a data.frame
+#'
+#' @param data data.frame
+#' @return character element wrapped in () reflecting the infered table spec
+#' @importFrom glue glue
+#' @export
+rs_create_table_spec <- function(data) {
+  data_types <- identify_rs_types(data)
   # Check table name
   if (grepl("-", table_name, fixed = TRUE)) {
     stop("Hyphen in table name not allowed")
   }
   # Identify and mutate column names
-  column_names <- sanitize_column_names_for_redshift(names(.data))
+  column_names <- sanitize_column_names_for_redshift(names(data))
 
-  spec <- paste(paste(column_names, data_types), collapse=", ")
-  sql_code <- whisker::whisker.render("CREATE TABLE IF NOT EXISTS {{table_name}} ({{spec}})")
-  DBI::dbExecute(dbcon, sql_code)
-  return(column_names)
+  return(glue("({paste(paste(column_names, data_types), collapse=', ')})"))
 }
