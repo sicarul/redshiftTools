a40745ec135c5bf17f28a8d0813d8b28bb34baac
Jake Russ <jake@redoakstrategic.com>
2020-04-22 18:22:22 +0000

    Add/clean up error handling for upload and delete operations

--- R/internal.R
+++ R/internal.R
@@ -10,8 +10,14 @@ uploadToS3 = function(data, bucket, split_files, key, secret, session, region){
 
   prefix = paste0(sample(rep(letters, 10),50), collapse = "")
 
-  if(!bucket_exists(bucket)){
+  resp <- bucket_exists(bucket)
+
+  if(is.null(attributes(resp))) {
+    # Do nothing
+  } else if(attributes(resp)$status_code %in% 404){
     stop("Bucket does not exist")
+  } else if(attributes(resp)$status_code %in% 403) {
+    stop("Access denied; please check AWS credentials")
   }
 
   splitted = suppressWarnings(split(data, seq(1:split_files)))
@@ -23,11 +29,28 @@ uploadToS3 = function(data, bucket, split_files, key, secret, session, region){
 
   upload_part = function(part, i){
 
-    s3Name <- paste(prefix, ".", formatC(i, width = 4, format = "d", flag = "0"), sep="")
+    s3Name <- paste(prefix, ".", formatC(i, width = 4, format = "d", flag = "0"), sep = "")
 
-    r=put_object(.data = part, bucket = bucket, key = s3Name)
+    # Put part on s3; retry 500 errors (three times)
+    upload_response <- NULL
+    attempt_count   <- 0
+
+    while (is.null(upload_response) | inherits(upload_response, "http_500") | attempt_count < 3){
+      attempt_count <- attempt_count + 1
+      upload_response <- tryCatch(put_object(.data = part, bucket = bucket, key = s3Name),
+                                  error = function(e) e)
+      if(inherits(upload_response, "http_500") & attempt_count < 3){
+        print(paste0("Request failed with 500 error and message: ", upload_response$message))
+        print("Retrying after two-second sleep")
+        Sys.sleep(2)
+      } else if (inherits(upload_response, "error")){
+        # Re-raise the error object on non-500 error or 500 error with maxed retries
+        stop(upload_response)
+      }
+    }
+
     pb$tick()
-    return(r)
+    return(upload_response)
   }
 
   res = map2(splitted, 1:split_files, upload_part)
@@ -52,7 +75,25 @@ deletePrefix = function(prefix, bucket, split_files, key, secret, session, regio
   pb$tick(0)
 
   deleteObj = function(key){
-    delete_object(bucket, key = key)
+
+    # Delete object from s3; retry 500 errors (three times)
+    delete_response <- NULL
+    attempt_count   <- 0
+
+    while (is.null(delete_response) | inherits(delete_response, "http_500") | attempt_count < 3){
+      attempt_count <- attempt_count + 1
+      delete_response <- tryCatch(delete_object(bucket, key = key),
+                                  error = function(e) e)
+      if(inherits(delete_response, "http_500") & attempt_count < 3){
+        print(paste0("Request failed with 500 error and message: ", delete_response$message))
+        print("Retrying after two-second sleep")
+        Sys.sleep(2)
+      } else if (inherits(delete_response, "error")){
+        # Re-raise the error object on non-500 error or 500 error with maxed retries
+        stop(delete_response)
+      }
+    }
+
     pb$tick()
   }
 
@@ -132,12 +173,9 @@ bucket_exists <- function(bucket) {
        )
      )
 
-  response <- tryCatch(
-    expr = svc$head_bucket(bucket),
-    error = function(e) NULL
-  )
+  response <- tryCatch(svc$head_bucket(bucket), error = function(e) e)
 
-  return(!is.null(response))
+  return(response)
 }
 
 #' @importFrom paws s3
@@ -186,10 +224,10 @@ delete_object <- function(bucket, key) {
 
   response <- tryCatch(
     expr = svc$delete_object(Bucket = bucket, Key = key),
-    error = function(e) NULL
+    error = function(e) e
   )
 
-  return(!is.null(response))
+  return(response)
 }
 
 #' @importFrom paws s3
@@ -214,8 +252,8 @@ put_object <- function(.data, bucket, key) {
              format_csv(na = "") %>%
              charToRaw() %>%
              svc$put_object(Body = ., Bucket = bucket, Key = key),
-    error = function(e) NULL
+    error = function(e) e
   )
 
-  return(!is.null(response))
+  return(response)
 }
